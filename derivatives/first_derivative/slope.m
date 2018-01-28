c67d337d9e595a8188f29b58cc396ceefb5d4433
Ramanathan Varadharajan <ramanathan.varadharajan@wur.nl>
2018-01-28 18:20:14 +0100

    Second order forward difference to find slope
    End points use 2dx as x2-x0 (This might be not an accurate way to descibe non-uniform grids)

---
+++ derivatives/first_derivative/slope.m
@@ -0,0 +1,34 @@
+%% Introduction to MATLAB script slope.m
+% First derivative of a function y(x) w.r.t a variable x is computed
+% y and x should be a vector of size m. 
+% [dy_dx]=slope(y,x,m)
+
+%% Definition of MATLAB function called slope
+
+function [dy_dx] = slope(y,x,m)
+
+%% Error checks
+    if (~isvector(y) || ~isvector(x))
+    error('Input variable must be a vector')
+    end
+    if ~isscalar(m)
+        error('Dimension of vector should be a scalar')
+    end
+
+%% Compution of derivative
+
+% Initialize the derivative vector to zero.
+    dy_dx=zeros(1,m);
+    
+% Forward difference (second order)
+    for i=1:m
+        if i==1
+            dy_dx(i) = (-3*y(i)+4*y(i+1)-y(i+2))/(x(i+2)-x(i));
+        elseif i==m
+            dy_dx(i) = (3*y(i)-4*y(i-1)+y(i-2))/(x(i)-x(i-2));
+        else
+            dy_dx(i) = (y(i+1)-y(i))/(x(i+1)-x(i));
+        end
+    end
+    
+end
